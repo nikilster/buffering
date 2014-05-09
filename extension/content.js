c1e2656db3a6b8f6f353b802a865de4a4129a6f0
Tim Luo <timoman@gmail.com>
2014-05-08 23:20:26 -0700

    Extension client listens for pushups and enables page after 10 pushups

--- extension/content.js
+++ extension/content.js
@@ -19,6 +19,7 @@ div.style.height = '100%';
 div.style.zIndex="2147483646";
 div.style.background="black"
 
+textDiv.id = 'textDiv';
 textDiv.style.top = '50%';
 textDiv.style.left = '50%';
 textDiv.style.width = '100%';
@@ -26,3 +27,13 @@ textDiv.style.height = '100%';
 textDiv.innerHTML="Hello, World!";
 textDiv.style.zIndex="2147483647";
 textDiv.style.color="white";
+
+var socket = io.connect('http://localhost:8080');
+var num_pushups = 0;
+socket.on('pushup', function (data) {
+  num_pushups++;
+  textDiv.innerHTML="Pushups done: " + num_pushups;
+  if (num_pushups >= 10) {
+    div.remove();
+  }
+});
